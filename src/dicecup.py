faa227df7eecbab25bc11e4a6be011e6d75f9c21
kujo-jotaro <fedloiacono23@gmail.com>
2021-08-03 10:06:54 -0300

    Added test suite for DiceCup class

--- src/dicecup.py
+++ src/dicecup.py
@@ -1,5 +1,5 @@
 from .sixsideddie import SixSidedDie
-from .exceptions import IndicesOutOfBoundsError
+from .exceptions import IndicesOutOfBoundsError, DiceAmountExceededError
 
 class DiceCup:
     """
@@ -13,6 +13,8 @@ class DiceCup:
         self.dice = self.roll(self.dice_amount)
 
     def roll(self, dice_amount):
+        if dice_amount > self.dice_amount:
+            raise DiceAmountExceededError
         dice = []
         for i in range(dice_amount):
             dice.append(SixSidedDie())
@@ -25,10 +27,13 @@ class DiceCup:
     
     def choose_dice(self, dice_indexes: "list[int]") -> "list[SixSidedDie]":
         if range(0, len(self.dice) - 1) not in dice_indexes:
-            raise IndicesOutOfBoundsError
+            illegal_indexes = [i for i \
+                in dice_indexes if i > len(self.dice) or i < 0]
+            raise IndicesOutOfBoundsError(illegal_indexes)
         staying_dice = []
         dice_indexes.sort()
         dice_indexes.reverse()
         for die_index in dice_indexes:
             staying_dice.append(self.dice.pop(die_index))
+        self.dice_amount = len(self.dice)
         return staying_dice
--- test/dicecup_tests.py
+++ test/dicecup_tests.py
@@ -1,10 +1,32 @@
 from src.dicecup import DiceCup
-from src.exceptions import IndicesOutOfBoundsError
+from src.exceptions import IndicesOutOfBoundsError, DiceAmountExceededError
+
+passed = True
 
 def pass_extra_indices():
+    global passed
+    dice_cup = DiceCup(5)
     indices = [1, 2, 3, 4, 5, 6]
-    dice_cup = DiceCup()
     try:
-        dice_cup.refill(indices)
-    except IndicesOutOfBoundsError:
-        exit()
+        dice_cup.choose_dice(indices)
+        passed &= False
+    except IndicesOutOfBoundsError as e:
+        print(e)
+
+def illegal_roll():
+    global passed
+    dice_cup = DiceCup(5)
+    try:
+        dice_cup.roll(6)
+        passed &= False
+    except DiceAmountExceededError as e:
+        print(e)
+
+def main():
+    global passed
+    pass_extra_indices()
+    illegal_roll()
+    return passed
+
+if __name__ == '__main__':
+    main()
